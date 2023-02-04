e0fa8a9509927dff3ba1a9c98e0bd5488a31e5c9
Prashant <prashantstha1110@gmail.com>
2023-02-04 10:40:24 +0545

    Initial Commit

---
+++ CountdownApp/CountdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownAppApp.swift
+//  CountdownApp
+//
+//  Created by Prashant shrestha on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
